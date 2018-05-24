5d7035b46a37c45935eca04f4dcb0cf68581eec5
marcoasoliveira <crazymaker2039@gmail.com>
2018-05-24 11:42:38 -0300

    remodelagem do layout. agora com bootstrap

--- js/main.js
+++ js/main.js
@@ -21,36 +21,79 @@ function exibeRepositorio(lista){
     
     lista.map( (repo) => {
 
-        let divItem = document.createElement('div')
-        divItem.classList.add('item-repositorio')
+        let card = document.createElement('div')
+        card.classList.add('card')
+        card.classList.add('col-sm-12')
+        card.classList.add('col-md-3')
 
-        let divIcon = document.createElement('div')
-        let iconTag = document.createElement('img')
-        iconTag.setAttribute('src','img/giticon.png')
-        divIcon.classList.add('iconTag')
-        divIcon.appendChild(iconTag)
+        card.setAttribute('id', 'card-works')
+
+        let imgCard = document.createElement('img')
+        imgCard.classList.add('card-img-top')
+        imgCard.setAttribute('src','img/giticon.png')
+
+        let cardBody = document.createElement('div')
+        cardBody.classList.add('card-body')
+        cardBody.classList.add('d-inline-block')
+        cardBody.classList.add('text-truncate')
+
+        let nomeRepositorio = document.createElement('h5')
+        nomeRepositorio.classList.add('card-title')
+        let nomeNode = document.createTextNode(`${repo.name}`)
+        nomeRepositorio.appendChild(nomeNode);
+
+        let descricaoRepositorio = document.createElement('span')
+        descricaoRepositorio.classList.add('card-text')
+        let descricaoNode = document.createTextNode(`${repo.description}`)
+        descricaoRepositorio.appendChild(descricaoNode)
+
+        let botao = document.createElement('a')
+        botao.setAttribute('href', `https://github.com/${repo.full_name}`)
+        botao.classList.add('btn')
+        botao.classList.add('btn-primary')
+        let botaoNode = document.createTextNode('Acesse')
+        botao.appendChild(botaoNode)
+
+        cardBody.appendChild(nomeRepositorio)
+        cardBody.appendChild(descricaoRepositorio)
+
+        card.appendChild(imgCard)
+        card.appendChild(cardBody)
+        card.appendChild(botao)
+
+        listaRepo.appendChild(card)
+
+        // let divItem = document.createElement('div')
+        // divItem.classList.add('item-repositorio')
+
+        // let divIcon = document.createElement('div')
+        // let iconTag = document.createElement('img')
+        // iconTag.setAttribute('src','img/giticon.png')
+        // divIcon.classList.add('iconTag')
+        // divIcon.appendChild(iconTag)
 
 
-        let divTitulo = document.createElement('div')
-        divTitulo.classList.add('item-header')
-        let linkTitulo = document.createElement('a')
-        let linkTexto = document.createTextNode(`${repo.name}`)
-        linkTitulo.appendChild(linkTexto);
-        linkTitulo.setAttribute('href', `https://github.com/${repo.full_name}`)
-        divTitulo.appendChild(linkTitulo)
+        // let divTitulo = document.createElement('div')
+        // divTitulo.classList.add('item-header')
+        // let linkTitulo = document.createElement('a')
+        // let linkTexto = document.createTextNode(`${repo.name}`)
+        // linkTitulo.appendChild(linkTexto);
+        // linkTitulo.setAttribute('href', `https://github.com/${repo.full_name}`)
+        // linkTitulo.setAttribute('target', '_blank')
+        // divTitulo.appendChild(linkTitulo)
 
-        let divDescricao = document.createElement('div')
-        divDescricao.classList.add('repo-descricao')
-        let descricao = document.createElement('p')
-        let descricaoTexto = document.createTextNode(`${repo.description}`)
-        descricao.appendChild(descricaoTexto)
-        divDescricao.appendChild(descricao)
+        // let divDescricao = document.createElement('div')
+        // divDescricao.classList.add('repo-descricao')
+        // let descricao = document.createElement('p')
+        // let descricaoTexto = document.createTextNode(`${repo.description}`)
+        // descricao.appendChild(descricaoTexto)
+        // divDescricao.appendChild(descricao)
 
-        divItem.appendChild(divIcon)
-        divItem.appendChild(divTitulo)
-        divItem.appendChild(divDescricao)
+        // divItem.appendChild(divIcon)
+        // divItem.appendChild(divTitulo)
+        // divItem.appendChild(divDescricao)
 
-        listaRepo.appendChild(divItem)
+        // listaRepo.appendChild(divItem)
 
     })
 
